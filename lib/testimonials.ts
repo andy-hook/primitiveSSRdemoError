b81b252e34c273abba129ce1c280b7c63c395c04
Michael Bensler <Mbensler@users.noreply.github.com>
2022-10-31 11:23:43 +0100

    Delete testimonials.ts

--- lib/testimonials.ts
+++
@@ -1,7 +0,0 @@
-export async function TestimonialsAPICall() {
-  // Call external API endpoint to get data
-  const res = await fetch('https://ck.directus.app/items/testimonials/');
-  const data = await res.json();
-
-  return data.data;
-}
