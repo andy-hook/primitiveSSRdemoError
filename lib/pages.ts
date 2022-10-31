1c4bd7b08a4472590bdbf76700f98f3807d9ccda
Michael Bensler <Mbensler@users.noreply.github.com>
2022-10-31 11:24:00 +0100

    Delete pages.ts

--- lib/pages.ts
+++
@@ -1,9 +0,0 @@
-export async function PageAPICall(url: string) {
-  // Call external API endpoint to get data
-  const res = await fetch(
-    `https://ck.directus.app/items/${url}?fields=*,translations.*,*,seo.*.*,*,contactPerson.*.*`
-  );
-  const data = await res.json();
-
-  return data.data;
-}
