e79f85a26ef2b17afc4087cf03f0b7f8a9994a53
Michael Bensler <Mbensler@users.noreply.github.com>
2022-10-31 11:25:01 +0100

    Delete lib directory

--- lib/fewFeatures.ts
+++
@@ -1,9 +0,0 @@
-export async function FewFeaturesAPICall() {
-  // Call external API endpoint to get data
-  const res = await fetch(
-    'https://ck.directus.app/items/fewFeatures?fields=*,translations.*,*,seo.*.*'
-  );
-  const data = await res.json();
-
-  return data.data;
-}
